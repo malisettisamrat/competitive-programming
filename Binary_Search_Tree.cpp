7d495dd9ad9ca6cdec0df2a1bd7f83676cd18ebf
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-18 01:29:55 +0530

    Update Binary_Search_Tree.cpp

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -1,1 +1,42 @@
 // Implementation of BINARY SEARCH TREE 
+
+struct Node{
+  int data;
+  struct Node *left,*right;
+};
+
+struct Node* New(int val)
+{
+    struct Node* tmp = new struct Node();
+    tmp -> data = val;
+    tmp -> right = tmp -> left = NULL;
+    
+    return tmp;
+}
+
+struct Node* insert(struct Node *root, int n)
+{
+    if( root == nullptr)
+          root = New(n);
+    
+        if(n > root->data)
+                root->right=insert(root->right,n);
+        else if(n < root->data)
+                root->left=insert(root->left,n);
+  
+    return root;
+    
+}
+
+void inorder(struct Node* root)
+{
+    if(root){
+      
+          inorder(root->left);
+
+          cout<<root->data<<" ";
+
+          inorder(root->right);
+    }
+}
+
