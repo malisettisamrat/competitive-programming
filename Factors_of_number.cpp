44e94767359fdecfcbeb0c48386aba694ab804d5
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-21 03:11:17 +0530

    Update Factors_of_number.cpp

--- Factors_of_number.cpp
+++ Factors_of_number.cpp
@@ -1,1 +1,16 @@
+// Finding Factors for a given number in sqrt(N) time
+vector<int>Factor;
 
+void Divisors(int n) 
+{ 
+    for (int i=1; i<=sqrt(n); i++) 
+    { 
+        if (n%i == 0) 
+        { 
+            if (n/i == i) 
+                Factor.push_back(i);
+            else 
+                Factor.push_back(n/i) , Factor.push_back(i); 
+        } 
+    } 
+} 
