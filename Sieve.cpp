b5ec7b0821281042393293a65c2071a505b2f8fc
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-21 03:08:15 +0530

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,1 +1,21 @@
+// pre-computation of prime using sieve of erosthenes
 
+const int maxN=1e8+9;
+
+bool prime[maxN];
+
+void sieve()
+{
+
+    fill(prime,prime+maxN,true);
+
+    prime[0] = prime[1] = false;
+    for(int i=2;i*i<maxN;i++)
+    {
+        if(prime[i])
+            for(int j=i*i;j<maxN;j+=i)
+                prime[j]=false;
+    }
+    
+}
+// call sieve in main() function
