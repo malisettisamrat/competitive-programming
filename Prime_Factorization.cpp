e65bf7e3ff672bfa2f95d4b484c38e8458d0170d
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-21 03:22:09 +0530

    Update Prime_Factorization.cpp

--- Prime_Factorization.cpp
+++ Prime_Factorization.cpp
@@ -1,1 +1,24 @@
 
+// Finding Prime Factors in sqrt(N) time
+
+vector<int>Factor;
+
+void primeFactors(int n)  
+{
+    while (n % 2 == 0)  
+    {  
+        Factor.push_back(2); 
+        n = n/2;  
+    }  
+  
+    for (int i = 3; i <= sqrt(n); i = i + 2)  
+    { 
+        while (n % i == 0)  
+        {  
+            Factor.push_back(i);  
+            n = n/i;  
+        }  
+    }  
+    if (n > 2)  
+        Factor.push_back(n);  
+}  
