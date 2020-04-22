45ed8037844ddc4d5389e7ab047feb409dccbd3a
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-22 11:11:28 +0530

    Update Prime_checking.cpp

--- Prime_checking.cpp
+++ Prime_checking.cpp
@@ -1,1 +1,12 @@
+// Checking Prime or not in Sqrt(N)
 
+bool prime(int n)
+{
+    if(n<=1)
+        return true;
+    for(int i=2;i*i<=n;i++)
+        if(n%i==0)
+            return false;
+    
+    return true;
+}
