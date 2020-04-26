ce9a3a8eb557320e4cf280c830e3cbf0e46f7184
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-26 19:41:37 +0530

    Update Binary_tree.cpp

--- Binary_tree.cpp
+++ Binary_tree.cpp
@@ -1,1 +1,32 @@
+// Implementation of Binary Tree 
 
+struct Node{
+  
+    int data ;
+    struct Node* right;
+    struct Node* left;
+
+};
+
+struct Node* New(int val)
+{
+    struct Node* tmp = new struct Node;
+    tmp -> data = val;
+    tmp -> right = tmp -> left = nullptr;
+    
+    return tmp;
+}
+
+Node* insert(vector<int>arr , Node* root, int i, int n) 
+{ 
+    if (i < n) 
+    { 
+        Node* temp = New(arr[i]); 
+        root = temp; 
+  
+        root->left = insert(arr, root->left, 2 * i + 1, n); 
+
+        root->right = insert(arr, root->right, 2 * i + 2, n); 
+    } 
+    return root; 
+} 
