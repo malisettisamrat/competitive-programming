3a327f2af2fdf92fd4e0cc351031a93f0f4a8fe3
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-04-30 01:53:50 +0530

    Create SSSP_Tree.cpp

---
+++ SSSP_Tree.cpp
@@ -0,0 +1,22 @@
+// .. Implementation of Single Source Shortest Path in c++
+
+vector< int > adj[10001];
+vector< bool > vis(10001);
+vector< int > dist(10001);
+
+void SSSP(int root , int dis)
+{
+    vis[root]=true;
+    dist[root]=dis;
+
+    for(auto x : adj[root])
+    {
+        if(vis[x]==false)
+        {
+            vis[x]=true;
+            SSSP(x,dist[root]+1);
+        }
+    }
+}
+
+// call the SSSP function in the main function with 1 as root
