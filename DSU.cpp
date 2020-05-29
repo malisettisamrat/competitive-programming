dc1508c877ff2132369fa2806e0342d35ba52285
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-05-29 18:37:56 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,1 +1,22 @@
+// find and union of DSU
 
+
+// FIND :-
+
+int find(int a)
+{
+    if (par[a] < 0)
+        return a;
+    else {
+        int x = find(par[a]);
+        par[a] = x;
+        return x;
+    }
+}
+
+// UNION :-
+
+void Union(int a , int b)
+{
+    par[b] = a;
+}
