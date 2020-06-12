0c2e72092284696c8a6b9a3e39b0b760f8ffb10a
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-06-12 22:00:35 +0530

    Update CycleInDirected.cpp

--- CycleInDirected.cpp
+++ CycleInDirected.cpp
@@ -1,1 +1,22 @@
+// cycle in Directed graph
 
+bool dfs(vector < int >adj[], int root, vector < bool > &vis, vector < bool >&par)
+{
+    vis[root] = true;
+    par[root] = true;
+
+    for(int child : adj[root])
+    {
+        if(!vis[child])
+        {
+            if(dfs(adj, child, vis, par))
+                return true;
+        }
+        else if(par[child] == true)
+            return true;
+    }
+
+    par[root] = false;
+
+    return false;
+}
