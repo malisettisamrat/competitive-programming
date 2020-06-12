c9d107f7a863f74cc0eb8e16c28be0c7101bd19e
malisettisamrat <44539602+malisettisamrat@users.noreply.github.com>
2020-06-12 21:58:13 +0530

    Update CycleInUndirected.cpp

--- CycleInUndirected.cpp
+++ CycleInUndirected.cpp
@@ -1,1 +1,19 @@
+// cycle detection in Undirected graph
 
+bool dfs(vector < int >adj[], int root, int par, vector < bool > &vis)
+{
+    vis[root] = true;
+
+    for(int child : adj[root])
+    {
+        if(!vis[child])
+        {
+            if(dfs(adj, child, root, vis))
+                return true;
+        }
+        else if(par != child)
+            return true;
+    }
+
+    return false;
+}
