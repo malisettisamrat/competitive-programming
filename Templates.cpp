c5b264109f06062a427685075b59cd9c2018ebcd
Samrat <samratmalisetti@gmail.com>
2021-08-02 11:55:33 +0530

    added new template

---
+++ Templates.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define debug(X) cerr << " [" << #X ": "; (print_(X)); cerr << "] ";
+template<class T> void print_(T X) { cerr << X; }
+template<class T> void print_(vector<T> vect) { for(T x : vect) { print_(x); cerr << ' '; } }
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	return 0;
+}
